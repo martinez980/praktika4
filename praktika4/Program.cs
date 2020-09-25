1db205d2dbdad8367dfed13f66f3f212afe0f085
student <student@C251U08>
2020-09-25 19:05:39 +0300

    Добавьте файлы проекта.

---
+++ praktika4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System;
+class Program
+{
+  static void Main(string[] args)
+  {
+    ArrayList myList = new ArrayList(); 
+    myList.Add("D");
+    myList.Add("Q");
+    myList.Add("W");
+    myList.Add("A");
+    foreach (string item in myList)
+    {
+      Console.WriteLine("Unsorted: {0}", item);
+    }
+    // Сортировка при помощи стандартного объекта сравнения 
+    myList.Sort();
+    foreach (string item in myList)
+    {
+      Console.WriteLine("   Sorted: {0}", item);
+    }
+  }
+}
